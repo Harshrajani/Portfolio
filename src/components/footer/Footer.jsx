1f14f02ae1c37ebdc2299e87ae46d7885f007ed0
Harsh <harsh0511r@gmail.com>
2024-01-11 22:37:55 +0530

    update links in footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,18 +15,18 @@ const Footer = () => {
             </ul>
 
             <div className="footer__social">
-                <a href="www.linkedin.com/in/harsh-rajani-72465b253" 
-                className="footer__social-link" target="_blank">
+                <a href="https://www.linkedin.com/in/harsh-rajani-72465b253" 
+                className="footer__social-link" >
                 <i className="uil uil-linkedin-alt"></i>
                 </a>
                 
-                <a href="www.github.com/Harshrajani" 
-                className="footer__social-link" target="_blank" >
+                <a href="https://www.github.com/Harshrajani" 
+                className="footer__social-link"  >
                     <i className="uil uil-github-alt"></i>
                 </a>
 
-                <a href="www.instagram.com/harsh_rajani__?igsh=ZGNjOWZkYTE3MQ==" 
-                className="footer__social-link" target="_blank">
+                <a href="https://www.instagram.com/harsh_rajani__?igsh=ZGNjOWZkYTE3MQ==" 
+                className="footer__social-link" >
                     <i className="uil uil-instagram"></i>
                 </a>
             </div>
