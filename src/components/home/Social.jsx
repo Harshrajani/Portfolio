ab01caa19f2119b923604e5c6636ea8dc5d54458
Harsh <harsh0511r@gmail.com>
2024-01-11 22:35:46 +0530

    correction in social links

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home_social">
-        <a href="www.google.com" 
-        className="home_social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/harsh-rajani-72465b253" 
+        className="home_social-icon" >
             <i className="uil uil-linkedin-alt"></i>
         </a>
         
-        <a href="www.github.com/Harshrajani" 
-        className="home_social-icon" target="_blank" >
+        <a href="https://www.github.com/Harshrajani" 
+        className="home_social-icon" >
              <i className="uil uil-github-alt"></i>
         </a>
 
-        <a href="www.instagram.com/harsh_rajani__?igsh=ZGNjOWZkYTE3MQ==" 
-        className="home_social-icon" target="_blank">
+        <a href="https://www.instagram.com/harsh_rajani__?igsh=ZGNjOWZkYTE3MQ==" 
+        className="home_social-icon" >
             <i className="uil uil-instagram"></i>
         </a>
     </div>
