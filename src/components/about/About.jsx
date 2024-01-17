c535964ef01a3870f7b7fc745653313f964aed5c
Harsh <harsh0511r@gmail.com>
2024-01-17 13:40:37 +0530

    New resume upload(2)

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import Aboutimg from "./picture.jpg"
-import resume from "./HarshRajani_.pdf"
+import resume from "./Harsh_Rajani_.pdf"
 import Info from './Info';
 const About = () => {
   return (
