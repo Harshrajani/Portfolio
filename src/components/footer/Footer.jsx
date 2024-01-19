84d02075f1ce05a87726c12f2cdc17580c9ec2e7
Harsh <harsh0511r@gmail.com>
2024-01-19 17:34:17 +0530

    footerLinks updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
         <div className="footer__container container">
             <h1 className="footer__title">Harsh</h1>
             <ul className='footer__list'>
-                <li><a href="/" className="footer__link">
+                <li><a href="#about" className="footer__link">
                   About  </a></li>
-                <li><a href="/" className="footer__link">
+                <li><a href="#portfolio" className="footer__link">
                   Projects  </a></li>
-                <li><a href="/" className="footer__link">
+                <li><a href="#skills" className="footer__link">
                     Skills</a></li>
             </ul>
 
